d8e5e6fa771a940b313fcd17bd3fa9a902091340
Natan Colenberg <natan.colenberg@gmail.com>
2019-07-09 20:58:48 +0300

    Add files via upload

--- StockPriceServer.js
+++ StockPriceServer.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var yahooStockPrices = require('yahoo-stock-prices');
 var app = express();
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    console.log(req.query);
+
+    yahooStockPrices.getCurrentPrice(req.query.symbol, function(err, price){
+
+    if(err)
+    {
+        console.error(err);
+        return;
+    }
+
+    console.log(price);
+    res.statusCode = 200;
+    res.send({'price': price});
+    });
+});
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
